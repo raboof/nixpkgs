40ba63a6484dc12e1181b6f396eb96e37aaee90c
Arnout Engelen <arnout@bzzt.net>
2020-07-14 08:28:13 +0200

    inkcut: init at 2.1.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20639,6 +20639,8 @@ in
   # Impressive, formerly known as "KeyJNote".
   impressive = callPackage ../applications/office/impressive { };
 
+  inkcut = libsForQt5.callPackage ../applications/misc/inkcut { };
+
   inkscape = callPackage ../applications/graphics/inkscape {
     lcms = lcms2;
   };
