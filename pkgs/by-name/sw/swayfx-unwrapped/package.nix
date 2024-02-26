2b8c27c7a2fa48a45b268bd2b8cea91f47e66d0d
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2024-02-25 21:34:15 -0500

    swayfx: remove patch handling for upstreamed patch

--- pkgs/by-name/sw/swayfx-unwrapped/package.nix
+++ pkgs/by-name/sw/swayfx-unwrapped/package.nix
@@ -32,18 +32,6 @@
     sha256 = "sha256-Gwewb0yDVhEBrefSSGDf1hLtpWcntzifPCPJQhqLqI0=";
   };
 
-  # This patch was backported into SwayFX
-  # remove when next release is rebased on Sway 1.9
-  patches =
-    let
-      removePatches = [
-        "LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.patch"
-      ];
-    in
-    builtins.filter
-      (patch: !builtins.elem (patch.name or null) removePatches)
-      (oldAttrs.patches or [ ]);
-
   meta = with lib; {
     description = "Sway, but with eye candy!";
     homepage = "https://github.com/WillPower3309/swayfx";
