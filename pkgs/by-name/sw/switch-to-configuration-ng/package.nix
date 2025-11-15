8f8a959b66ed39b27f2d1a4a83868a890262b19a
Arnout Engelen <arnout@bzzt.net>
2025-11-15 22:05:48 +0100

    wtf

--- pkgs/by-name/sw/switch-to-configuration-ng/package.nix
+++ pkgs/by-name/sw/switch-to-configuration-ng/package.nix
@@ -19,13 +19,16 @@ rustPlatform.buildRustPackage {
   buildInputs = [ dbus ];
 
   postFixup = ''
-    mv $out/bin/switch-to-configuration $out/bin/.switch-wrapped
-    cat >$out/bin/switch-to-configuration <<EOF;
-#!/usr/bin/env sh
-
-$out/bin/.switch-wrapped <&0 >&1 2>/tmp/out.txt
-EOF
+    # tralala
   '';
+#  postFixup = ''
+#    mv $out/bin/switch-to-configuration $out/bin/.switch-wrapped
+#    cat >$out/bin/switch-to-configuration <<EOF;
+##!/usr/bin/env sh
+#
+#$out/bin/.switch-wrapped <&0 >&1 2>/tmp/out.txt
+#EOF
+#  '';
 
   env.SYSTEMD_DBUS_INTERFACE_DIR = "${buildPackages.systemd}/share/dbus-1/interfaces";
 
